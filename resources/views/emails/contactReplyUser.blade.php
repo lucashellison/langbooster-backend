00fca0d1254c10789a515ec081d925691a978d1c
Lucas Camargo <lhcamargo3@gmail.com>
2024-03-03 09:48:33 -0300

    Update user contact notification message

--- resources/views/emails/contactReplyUser.blade.php
+++ resources/views/emails/contactReplyUser.blade.php
@@ -4,5 +4,5 @@
 We've successfully received your message and want to thank you for writing to us. We will look over your message and get back to you shortly.
 
 Talk to you soon,
-The LangBooster Support Team
+The LangBooster Support Team.
 @endcomponent
